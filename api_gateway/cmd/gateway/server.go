b3f32aba8b335214c91701a8025f2ef9c957ab27
Stefan Andrei <andreistefanciprian@gmail.com>
2025-08-27 09:10:56 +0930

    add api_gateway

--- api_gateway/cmd/gateway/server.go
+++ api_gateway/cmd/gateway/server.go
@@ -1,19 +1,201 @@
 package main
 
-import "net/http"
+import (
+	"context"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"strings"
+
+	authpb "github.com/andreistefanciprian/gomicropay/api_gateway/auth"
+	mmpb "github.com/andreistefanciprian/gomicropay/api_gateway/money_movement"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var mmClient mmpb.MoneyMovementServiceClient
+var authClient authpb.AuthServiceClient
 
 func main() {
+	// auth connection
+	authConn, err := grpc.Dial("auth:9000", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := authConn.Close(); err != nil {
+			log.Println("Failed to close auth connection:", err)
+		}
+	}()
+
+	authClient = authpb.NewAuthServiceClient(authConn)
+
+	// money movement connection
+	mmConn, err := grpc.Dial("money_movement:7000", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := mmConn.Close(); err != nil {
+			log.Println("Failed to close money movement connection:", err)
+		}
+	}()
+	mmClient = mmpb.NewMoneyMovementServiceClient(mmConn)
+
 	http.HandleFunc("/login", login)
 	http.HandleFunc("/customer/payment/authorize", customerPaymentAuthorize)
 	http.HandleFunc("/customer/payment/capture", customerPaymentCapture)
-	http.HandleFunc("/customer/payment/ledger", customerLedger)
 
 }
 
-func login(w http.ResponseWriter, r *http.Request) {}
+func login(w http.ResponseWriter, r *http.Request) {
+	username, password, ok := r.BasicAuth()
+	if !ok {
+		http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+		return
+	}
 
-func customerPaymentAuthorize(w http.ResponseWriter, r *http.Request) {}
+	ctx := context.Background()
+	token, err := authClient.GetToken(ctx, &authpb.Credentials{
+		UserName: username,
+		Password: password,
+	})
+	if err != nil {
+		_, writeErr := w.Write([]byte(err.Error()))
+		if writeErr != nil {
+			log.Println(writeErr)
+		}
+		return
+	}
+	_, err = w.Write([]byte(token.Jwt))
+	if err != nil {
+		log.Println("Failed to write token:", err)
+	}
+}
 
-func customerPaymentCapture(w http.ResponseWriter, r *http.Request) {}
+func customerPaymentAuthorize(w http.ResponseWriter, r *http.Request) {
+	authHeader := r.Header.Get("Authorization")
+	if authHeader == "" {
+		http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+		return
+	}
 
-func customerLedger(w http.ResponseWriter, r *http.Request) {}
+	if !strings.HasPrefix(authHeader, "Bearer ") {
+		http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+		return
+	}
+
+	token := strings.TrimPrefix(authHeader, "Bearer ")
+	ctx := context.Background()
+	_, err := authClient.ValidateToken(ctx, &authpb.Token{Jwt: token})
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+		return
+	}
+
+	// Call money movement service
+	type authorizePayload struct {
+		CustomerWalletUserId string `json:"customer_wallet_user_id"`
+		MerchantWalletUserId string `json:"merchant_wallet_user_id"`
+		Cents                int64  `json:"cents"`
+		Currency             string `json:"currency"`
+	}
+
+	var payload authorizePayload
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	err = json.Unmarshal(body, &payload)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	ctx = context.Background()
+	authorizedResponse, err := mmClient.Authorize(ctx, &mmpb.AuthorizePayload{
+		CustomerWalletUserId: payload.CustomerWalletUserId,
+		MerchantWalletUserId: payload.MerchantWalletUserId,
+		Cents:                payload.Cents,
+		Currency:             payload.Currency,
+	})
+	if err != nil {
+		_, writeErr := w.Write([]byte(err.Error()))
+		if writeErr != nil {
+			log.Println(writeErr)
+		}
+		return
+	}
+
+	type response struct {
+		Pid string `json:"pid"`
+	}
+
+	resp := response{
+		Pid: authorizedResponse.Pid,
+	}
+	responseJSON, err := json.Marshal(resp)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, err = w.Write(responseJSON)
+	if err != nil {
+		log.Println("Failed to write response:", err)
+	}
+}
+
+func customerPaymentCapture(w http.ResponseWriter, r *http.Request) {
+	authHeader := r.Header.Get("Authorization")
+	if authHeader == "" {
+		http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+		return
+	}
+
+	if !strings.HasPrefix(authHeader, "Bearer ") {
+		http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+		return
+	}
+
+	token := strings.TrimPrefix(authHeader, "Bearer ")
+	ctx := context.Background()
+	_, err := authClient.ValidateToken(ctx, &authpb.Token{Jwt: token})
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+		return
+	}
+
+	// Call money movement service
+	type capturePayload struct {
+		Pid string `json:"pid"`
+	}
+
+	var payload capturePayload
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	err = json.Unmarshal(body, &payload)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	ctx = context.Background()
+	_, err = mmClient.Capture(ctx, &mmpb.CapturePayload{Pid: payload.Pid})
+	if err != nil {
+		_, writeErr := w.Write([]byte(err.Error()))
+		if writeErr != nil {
+			log.Println(writeErr)
+		}
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
